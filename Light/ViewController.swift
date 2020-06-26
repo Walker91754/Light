54c95a4d9c2fbfd0aecbe7df36069bf0216916db
Walker Huang (IS-TW) <walker_huang@trendmicro.com>
2020-06-26 15:50:08 +0800

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Walker_MAC on 2020/6/26.
+//  Copyright © 2020 Walker_MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
